7ddda0f78de5b72b5e705c1ec5ac48bd9840a7c2
HongFe <bbcyo@naver.com>
2022-12-02 01:20:21 -0800

    update

--- app/language understand/test.py
+++ app/language understand/test.py
@@ -1,11 +1,8 @@
-
-
-
 class TEXT_ANALYTICS:
     def __init__(self):
         pass
 
     def pre_handling(self,sentence):
         ## 문장 가공 불필요시 X
-        fixed_sentence=sentence
-        return fixed_sentence
+        self.fixed_sentence=sentence
+        return self.fixed_sentence
